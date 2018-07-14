791f7210d69277a9c46937ce4d7e64a6c3918d6b
Caio <alcantaracaiolucas@gmail.com>
2018-07-14 17:57:32 +0100

    Add LSTM config to better performance

--- src/brain/index.js
+++ src/brain/index.js
@@ -8,8 +8,14 @@ import { train as runCongnitiveServices, classifySubjects } from './cognition'
 
 const setup = async (LSTM = false) => {
   console.log('Initializing brain...')
-  const network = new Brain.NeuralNetwork()
-  await network.train(serializer.processTrainingData(trainingData))
+  const network = new Brain.recurrent.LSTMTimeStep()
+  await network.train(serializer.processTrainingData(trainingData),  {
+    learningRate: 0.3,    // scales with delta to effect training rate --> number between 0 and 1
+    momentum: 0.1,        // scales with next layer's change value --> number between 0 and 1
+    callback: null,       // a periodic call back that can be triggered while training --> null or function
+    callbackPeriod: 10,   // the number of iterations through the training data between callback calls --> number greater than 0
+    timeout: Infinity  
+  })
   
   console.log('Neural network, basic training')
   
