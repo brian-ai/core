3682c17d23256e787435901bcbd9de18322e5aa9
Caio <alcantaracaiolucas@gmail.com>
2018-07-27 06:39:15 +0100

    Fix minor issues

--- src/brain/communication/speaker/index.js
+++ src/brain/communication/speaker/index.js
@@ -1,16 +1,10 @@
 import AWS from 'aws-sdk'
-import { Readable } from 'stream'
+import Stream from 'stream'
 import Speaker from 'speaker'
-
-const speaker = new Speaker({
-  channels: 1,
-  bitDepth: 16,
-  sampleRate: 17350
-})
 
 const Polly = new AWS.Polly({
   signatureVersion: 'v4',
-  region: 'us-east-1',
+  region: 'us-east-1'
 })
 
 const createSentence = (sentence) => ({
@@ -18,36 +12,30 @@ const createSentence = (sentence) => ({
   'OutputFormat': 'pcm',
   'TextType': 'ssml',
   'VoiceId': 'Brian',
-});
+})
 
-const speak = async (phrase, nextStep) => {
-  await Polly.synthesizeSpeech(createSentence(phrase), function(err, res) {
+const speaker = new Speaker({
+    channels: 1,
+    bitDepth: 16,
+    sampleRate: 16000
+})
+
+const speak = (phrase) => {
+  return Polly.synthesizeSpeech(createSentence(phrase), function(err, res) {
     if (err) {
-      console.log('err', err)
+        console.log('err', err)
     } else if (res && res.AudioStream instanceof Buffer) {
-      try {
-        let bufferStream = new Readable
-        let start = 0
-        const bufferLength = 255
-        
-        do {
-          const piece = res.AudioStream.slice(start, bufferLength)
-          bufferStream.push(piece)
-          start += bufferLength
+        try {
+          let bufferStream = new Stream.PassThrough()
+          bufferStream.end(res.AudioStream)
+          bufferStream.pipe(speaker);
+        } catch (err) {
+          console.error(err);
         }
-        while (res.AudioStream.length < bufferLength);
-
-        bufferStream.pipe(speaker)
-          .on('end', () => {
-            console.log('xpto')
-          })
-      } catch (err) {
-        console.error(err)
-      }
     }
   })
 }
 
 export {
   speak
-} 
+}
