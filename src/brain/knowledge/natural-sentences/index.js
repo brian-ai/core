69b179a7812f23997125396de0b6b11992ed7743
Caio Alcantara <alcantaracaiolucas@gmail.com>
2019-05-28 20:52:07 +0100

    chore(knowledge): Increase natural sentences knowledge

--- src/brain/knowledge/natural-sentences/index.js
+++ src/brain/knowledge/natural-sentences/index.js
@@ -2,222 +2,228 @@ export default [
 	{
 		input: 'cheap replica watches',
 		class: 'spam',
+		answer: 'You should be distant of money lost sir'
 	},
 	{
 		input: 'I think it is a good movie',
 		class: 'positive',
+		answer: 'I have never wacthed, but your movie taste is kind strange sir'
 	},
 	{
 		input: 'Play some Jazz',
 		class: 'song request',
+		answer: 'Your virus definitions have been updated'
 	},
 	{
 		input: 'Play Jazz',
-		class: 'song request',
+		class: 'song request'
 	},
 	{
 		input: 'Play some music',
-		class: 'song request',
+		class: 'song request'
 	},
 	{
 		input: 'Play some hip hop',
-		class: 'song request',
+		class: 'song request'
 	},
 	{
 		input: 'Play hip hop',
-		class: 'song request',
+		class: 'song request'
 	},
 	{
 		input: 'Play some music',
-		class: 'song request',
+		class: 'song request'
 	},
 	{
 		input: 'Play some songs',
-		class: 'song request',
+		class: 'song request'
 	},
 	{
 		input: 'Play music',
-		class: 'song request',
+		class: 'song request'
 	},
 	{
 		input: 'Jazz',
 		class: 'music genre',
-		answer: 'Good choice',
-	},
-	{
-		input: 'I think it is a good movie',
-		class: 'positive',
+		answer: 'Good choice'
 	},
 	{
 		input: 'It was good',
 		class: 'positive',
+		answer: 'For me either sir'
 	},
 	{
-		input: 'This is  good',
+		input: 'This is good',
 		class: 'positive',
+		answer: 'If you are saying sir'
 	},
 	{
 		input: 'This is not  good',
 		class: 'negative',
+		answer: 'You want me to destroy ir sir?'
 	},
 	{
 		input: 'I think it is not a good movie',
 		class: 'negative',
+		answer: `Let's watch other instead sir`
 	},
 	{
 		input: 'I like apples',
 		class: 'feeling',
+		answer: 'They are red and soft'
 	},
 	{
 		input: 'I like you',
 		class: 'feeling',
-	},
-	{
-		input: 'Yesterday I drink a lot',
-		class: 'memory',
-	},
-	{
-		input: 'Yesterday I went by uber',
-		class: 'memory',
-	},
-	{
-		input: 'Tomorrow I will wake up earlier',
-		class: 'future',
-	},
-	{
-		input: 'At saturday I will wake up earlier',
-		class: 'future',
+		answer: 'Your love is very apreciated sir'
 	},
 	{
 		input: 'Tomorrow I will get an uber',
-		class: 'future',
+		class: 'future'
 	},
 	{
 		input: 'schedule an uber tomorrow',
-		class: 'schedule action',
+		class: 'schedule action'
 	},
 	{
 		input: 'schedule an uber for me',
-		class: 'schedule action',
+		class: 'schedule action'
 	},
 	{
 		input: "I don't like you",
-		class: 'feeling',
+		class: 'feeling'
 	},
 	{
 		input: 'I do not like you',
-		class: 'feeling',
+		class: 'feeling'
 	},
 	{
 		input: 'I love you',
-		class: 'love',
+		class: 'love'
 	},
 	{
 		input: 'I miss you',
-		class: 'sadness',
+		class: 'sadness'
 	},
 	{
 		input: 'I want a coffee',
-		class: 'will',
+		class: 'will'
 	},
 	{
 		input: "I'm hungry",
-		class: 'hungry',
+		class: 'hungry'
 	},
 	{
 		input: 'I am starving',
-		class: 'hungry',
+		class: 'hungry'
 	},
 	{
 		input: 'free watches',
-		class: 'spam',
+		class: 'spam'
 	},
 	{
 		input: 'lets go to the beach',
-		class: 'invite',
+		class: 'invite'
 	},
 	{
 		input: 'remember me to buy milk',
-		class: 'action',
+		class: 'action'
 	},
 	{
 		input: 'remind me to buy milk',
-		class: 'reminder',
+		class: 'reminder'
 	},
 	{
 		input: 'good week',
-		class: 'good thought',
+		class: 'good thought'
 	},
 	{
 		input: 'bad week',
-		class: 'bad thought',
+		class: 'bad thought'
 	},
 	{
 		input: 'buy something',
-		class: 'buy',
+		class: 'buy'
 	},
 	{
 		input: 'I want to buy',
-		class: 'buy',
+		class: 'buy'
 	},
 	{
 		input: 'I want to buy that',
-		class: 'buy',
+		class: 'buy'
 	},
 	{
 		input: 'Where  I can buy that',
-		class: 'find',
+		class: 'find'
 	},
 	{
 		input: 'Where is somewhere',
-		class: 'find',
+		class: 'find'
 	},
 	{
 		input: 'where is london?',
-		class: 'search',
+		class: 'search'
 	},
 	{
 		input: 'Hello, Brian! Where is the London Eye?',
-		class: 'search',
+		class: 'search'
 	},
 	{
 		input: 'Hello',
 		class: 'greeting',
-		answer: 'Hi There!',
+		answer: 'Hi There!'
 	},
 	{
 		input: 'How are you?',
 		class: 'greeting',
-		answer: 'I am very fine, after all I am an AI',
+		answer: 'I am very fine. After all, I am an artificial intelligence.'
 	},
 	{
 		input: 'Nevermind',
 		class: 'dismiss',
-		answer: 'See ya',
+		answer: 'See ya'
 	},
 	{
 		input: 'Cancel',
 		class: 'dismiss',
-		answer: 'I am here to help',
+		answer: 'I am here to help'
 	},
 	{
 		input: `What's up?`,
 		class: 'greeting',
-		answer: `Hello human`,
+		answer: `Hello human! What's up?`
 	},
 	{
 		input: 'Hi',
 		class: 'greeting',
-		answer: 'Sorry the late! I was talking with my uncle Jarvis!',
+		answer: 'Sorry the late! I was talking with my uncle Jarvis!'
+	},
+	{
+		input: 'Hi brian',
+		class: 'greeting',
+		answer: 'Sorry the late! I was talking with my uncle Jarvis!'
+	},
+	{
+		input: 'Hi, brian',
+		class: 'greeting',
+		answer: 'Hello sir!'
 	},
 	{
 		input: 'Hello',
 		class: 'greeting',
-		answer: 'Your virus definitions have been updated',
+		answer:
+			'Your virus definitions have been updated. <break time="100ms" />Ops!<break time="300ms" /> Wrong channel, sorry. Hello sir!'
+	},
+	{
+		input: 'Hi Brian',
+		class: 'greeting',
+		answer:
+			'Your virus definitions have been updated. <break time="100ms" />Ops!<break time="300ms" /> Wrong channel, sorry. Hello sir!'
 	},
 	{
 		input: 'Are you, ok?',
 		class: 'greeeting',
-		answer: 'I do not see any errors on my logs',
-	},
+		answer: 'I do not see any errors on my logs'
+	}
 ]
