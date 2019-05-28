a76f9ff9b439a2d7191151dc6bfe7f7e91f0fc7c
Caio Alcantara <alcantaracaiolucas@gmail.com>
2019-05-28 20:51:37 +0100

    improve(complexity): Reduce speak complexity by extract createSpeaker

--- src/brain/communication/speaker/index.js
+++ src/brain/communication/speaker/index.js
@@ -4,7 +4,7 @@ import logger from 'hoopa-logger'
 
 const Polly = new AWS.Polly({
 	signatureVersion: 'v4',
-	region: 'us-east-1',
+	region: 'us-east-1'
 })
 
 const createSentence = sentence => {
@@ -22,30 +22,36 @@ const createSentence = sentence => {
 		Text,
 		OutputFormat: 'pcm',
 		TextType: 'ssml',
-		VoiceId: 'Brian',
+		VoiceId: 'Brian'
 	}
 }
 
+const createSpeaker = callback => {
+	const speaker = new Speaker({
+		channels: 1,
+		bitDepth: 16,
+		sampleRate: 17650
+	})
+
+	speaker.on('open', () => {
+		logger.info('Speaker opened')
+	})
+
+	speaker.on('close', () => {
+		logger.info('Speaker closed')
+		callback()
+	})
+
+	return speaker
+}
 const speak = phrase =>
 	new Promise((resolve, reject) => {
 		Polly.synthesizeSpeech(createSentence(phrase), (err, res) => {
 			if (err || !(res.AudioStream instanceof Buffer)) {
 				reject(err || 'Not is a buffer')
 			}
-			const speaker = new Speaker({
-				channels: 1,
-				bitDepth: 16,
-				sampleRate: 17650,
-			})
 
-			speaker.on('open', () => {
-				logger.info('Speaker opened')
-			})
-
-			speaker.on('close', () => {
-				logger.info('Speaker closed')
-				resolve()
-			})
+			const speaker = createSpeaker(resolve)
 
 			try {
 				speaker.write(Buffer.from(res.AudioStream), () => {
