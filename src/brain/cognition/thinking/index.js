5450976b751e43cdfacc8a614b5da0fd1ef36d05
Caio Alcantara <alcantaracaiolucas@gmail.com>
2019-07-13 07:09:39 +0100

    Lint fix

--- src/brain/cognition/thinking/index.js
+++ src/brain/cognition/thinking/index.js
@@ -4,15 +4,15 @@ const processIntentType = ({ label: intentType }, text) => {
 	// TODO: Begin NLP Processing based on intent type
 
 	switch (intentType) {
-		case 'song request': {
-			return RabbitMQ.sendMessage(
-				'music_service',
-				`{ "data": "${text}", "options": { "play": true } }`
-			)
-		}
-		default:
-			console.log(intentType)
-			break
+	case 'song request': {
+		return RabbitMQ.sendMessage(
+			'music_service',
+			`{ "data": "${text}", "options": { "play": true } }`
+		)
+	}
+	default:
+		console.log(intentType)
+		break
 	}
 
 	return false
